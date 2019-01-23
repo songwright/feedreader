a2b5fa9021df115308ef8d988a8faa3149b50d44
Ramon Sanchez <songwright66@gmail.com>
2019-01-23 11:17:57 -0800

    docs: Update menu test suite comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,9 +27,9 @@ $(function() {
         });
 
 
-        /* This is a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* This is a test that loops through each feed in the
+         * allFeeds object and ensures it has a URL defined and
+         * that the URL is not empty.
          */
         it('should define urls', function() {
             for(let feed of allFeeds) {
@@ -38,9 +38,9 @@ $(function() {
             }
         });
 
-        /* This is a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* This is a test that loops through each feed in the
+         * allFeeds object and ensures it has a name defined and
+         * that the name is not empty.
          */
         it('should define feed names', function() {
             for(let feed of allFeeds) {
@@ -51,13 +51,11 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* This test suite tests the menu */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This a test that ensures the menu element is hidden
+         * by default.
          */
         it('should be hidden', function() {
             // const body = document.querySelector('body');
@@ -65,10 +63,10 @@ $(function() {
             expect(document.body.classList.contains('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+        /* This is a test that ensures the menu changes visibility
+         * when the menu icon is clicked. This test has two
+         * expectations: does the menu display when clicked and
+         * does it hide when clicked again.
          */
         it('should click on and off', function() {
             // const body = document.querySelector('body');
