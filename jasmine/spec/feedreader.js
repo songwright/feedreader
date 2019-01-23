1d7e600330cec3e9cc4552a53705e4a2457cd769
Ramon Sanchez <songwright66@gmail.com>
2019-01-23 11:05:57 -0800

    docs: Update RSS feed comments for define urls

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
